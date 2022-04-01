690b4c7d5c2816bdf47c4c99176532f906a42687
adidui <adilev3344@gmail.com>
2022-04-01 11:16:53 +0300

    add resizer

--- src/pages/demo/demo.js
+++ src/pages/demo/demo.js
@@ -4,9 +4,44 @@ import { Dropdown } from '../../components/Dropdowns/dropdownV1/Dropdown';
 import { Heading } from '../../components/heading/heading';
 import Row from '../../Grid/row/row';
 import Col from '../../Grid/cols/Col';
-
+import Resizable from '../../resizer';
+import { createRef } from '../../../jsx-render/client/client';
 export const Demo = (props) => {
     const { querySelector } = props;
+
+    const blocks = [<div className="selected-block adi-block">
+    <div className="selected-element ">
+        <h1 className="heading-h1 block-element ">
+            Hello I'm <span className="text-primary">Adi L.</span><br />
+        </h1>
+        <p className="block-element ">And I'm a Web Developer</p>
+        <Dropdown pose="top" color="primary" className="about-me-btn" title="About Me">
+            <div className="about-me-modal">
+                <Row justify="flex-start" alignItems="center">
+                    <Heading style={{ color: "var(--primary)" }}>Hi I'm Adi,</Heading>
+                </Row>
+                <div style={{ margin: "30px 0" }}>
+                    <p>
+                        Frontend is all my passions, especially create creative tools  for the web with love and unique inventions.
+                    </p>
+                    <p>
+                        Creativity is my guiding light and it always my goal.
+                    </p>
+                    <p>
+                        my specialties include quickly learning new skills and solving problems, website optimization.<br/>
+                        I Love to use JavaScript, JSX, React, Webpack, HTML, CSS, NodeJS, C#, SQL, MongoDB, GitHub/Git
+                    </p>
+                </div>
+
+                <Row justify="flex-start" >
+                    <Col title="coffee" sm={1}>☕ |</Col>
+                    <Col title="code" sm={1}>👨‍💻 |</Col>
+                    <Col title="music" sm={1}>🎵</Col>
+                </Row>
+            </div>
+        </Dropdown>
+    </div>
+</div>]
     const myComponent = new Hooks({
         onLoad: (target) => {
             target.style.height = "100%";
@@ -14,57 +49,39 @@ export const Demo = (props) => {
             const container = document.querySelector(querySelector);
             renderRules(container);
             window.addEventListener("resize", OnResize);
+            new Resizable(".adi-block", {
+                container: document.body,
+                minWidth: 100,
+                onResize(element, { width, height, left, top }) {
+                    if (width) {
+                        element.style.width = width;
+                        element.style.maxWidth = "100%";
+                    }
+                    if (height) {
+                        element.style.height = "auto";
+                        element.style.minHeight = height
+                    }
+                    if(left) {
+                        element.style.left = left;
+                    }
+                    if(top){
+                        element.style.top = top;
+                    }
+                }
+            
+            })
         },
         children: <div style={{ height: "100%", width: "100%" }}>
             <style>
                 {style}
             </style>
-
-
-            <div className="selected-block adi-block">
-
-                <div className="selected-element ">
-                    <h1 className="heading-h1 block-element ">
-                        Hello I'm <span className="text-primary">Adi L.</span><br />
-                    </h1>
-                    <p className="block-element ">And I'm a Web Developer</p>
-                    <Dropdown pose="top" color="primary" className="about-me-btn" title="About Me">
-                        <div className="about-me-modal">
-                            <Row justify="flex-start" alignItems="center">
-                                <Heading style={{ color: "var(--primary)" }}>Hi I'm Adi,</Heading>
-                            </Row>
-                            <div style={{ margin: "30px 0" }}>
-                                <p>
-                                    Frontend is all my passions, especially create creative tools  for the web with love and unique inventions.
-                                </p>
-                                <p>
-                                    Creativity is my guiding light and it always my goal.
-                                </p>
-                                <p>
-                                    my specialties include quickly learning new skills and solving problems, website optimization.<br/>
-                                    I Love to use JavaScript, JSX, React, Webpack, HTML, CSS, NodeJS, C#, SQL, MongoDB, GitHub/Git
-                                </p>
-                            </div>
-
-                            <Row justify="flex-start" >
-                                <Col title="coffee" sm={1}>☕ |</Col>
-                                <Col title="code" sm={1}>👨‍💻 |</Col>
-                                <Col title="music" sm={1}>🎵</Col>
-                            </Row>
-                        </div>
-                    </Dropdown>
-                    <div className="dot dot-top-left"></div>
-                    <div className="dot dot-top-right"></div>
-                    <div className="dot dot-bottom-left "></div>
-                    <div className="dot dot-bottom-right"></div>
-                </div>
-
-
-            </div>
-
-
+            {
+                blocks.map(el=>{
+                    return el;
+                })
+            }
+            
         </div>
-
     })
     const Rule = ({ height, bottom, width, left, top, className, children }) => <div className={className} style={{ height: height, bottom: bottom, width: width, left: left, top: top }}>
         {children}
---
+++ src/resizer/index.js
@@ -0,0 +1,232 @@
+import classes from "./style.module.css";
+
+class Resizable {
+    constructor(querySelector, options) {
+        const { container = document.body, onResize, minWidth } = options;
+        this.querySelector = querySelector;
+        this.container = container;
+        this.isImage = false;
+        this.onResize = onResize;
+        this.minimum_size = minWidth;
+        this.resizers = [];
+        this.clickedResizer = null;
+        this.element;
+        this.isMouseDown = false;
+        this.original_width = 0;
+        this.original_height = 0;
+        this.original_x = 0;
+        this.original_y = 0;
+        this.original_mouse_x = 0;
+        this.original_mouse_y = 0;
+        this.resize = this.resize.bind(this);
+        this.stopResize = this.stopResize.bind(this);
+        this.onClickedTarget = this.onClickedTarget.bind(this);
+        this.rePosition = this.rePosition.bind(this);
+        this.init = this.init.bind(this);
+        this.onClickResizer = this.onClickResizer.bind(this);
+        this.init();
+    }
+    onClickResizer(e) {
+        this.clickedResizer = e.target;
+        e.preventDefault()
+        this.isMouseDown = true;
+        this.original_width = parseFloat(getComputedStyle(this.element, null).getPropertyValue('width').replace('px', ''));
+        this.original_height = parseFloat(getComputedStyle(this.element, null).getPropertyValue('height').replace('px', ''));
+        this.original_x = this.element.getBoundingClientRect().left;
+        this.original_y = this.element.getBoundingClientRect().top;
+        this.original_mouse_x = e.pageX;
+        this.original_mouse_y = e.pageY;
+        window.addEventListener('mousemove', this.resize)
+        window.addEventListener('mouseup', this.stopResize)
+    }
+    createSizePreview() {
+        const sizePreview = document.createElement('div');
+        sizePreview.classList.add(classes.sizePreview);
+        return sizePreview;
+    }
+
+    createBorder(className) {
+        const border = document.createElement('div');
+        border.classList.add(classes.resizableBorder);
+        border.classList.add(className);
+        return border;
+    }
+    init() {
+        this.sizePreview = this.createSizePreview();
+        this.borders = [this.createBorder('left'), this.createBorder('right'), this.createBorder('bottom'), this.createBorder('top')];
+        const resizers = this.resizers = [this.createResizer('bottom-center'), this.createResizer('left-center'), this.createResizer('right-center'), this.createResizer('bottom-left'), this.createResizer('bottom-right'), this.createResizer('top-right'), this.createResizer('top-left')];
+        for (let i = 0; i < resizers.length; i++) {
+            resizers[i].addEventListener('mousedown', this.onClickResizer);
+        }
+        window.addEventListener("mousedown", this.onClickedTarget)
+    }
+    onClickedTarget(event) {
+        if (this.isMouseDown) return;
+        const clickOn = event.path[0];
+        const target = clickOn.closest(this.querySelector);
+        if (target) {
+            this.resizers.forEach(resizer => document.body.appendChild(resizer));
+            this.borders.forEach(border => document.body.appendChild(border));
+            this.element = target;
+            this.rePosition();
+            this.container.addEventListener("scroll", this.rePosition);
+            window.addEventListener("scroll", this.rePosition);
+            window.addEventListener("resize", this.rePosition);
+        } else {
+            this.resizers.forEach(resizer => resizer.remove());
+            this.borders.forEach(border => border.remove());
+            this.container.removeEventListener("scroll", this.rePosition);
+            window.removeEventListener("scroll", this.rePosition);
+            window.removeEventListener("resize", this.rePosition);
+        }
+    }
+    stopResize() {
+        this.isMouseDown = false;
+        this.sizePreview.remove();
+        window.removeEventListener('mouseup', this.stopResize)
+        window.removeEventListener('mousemove', this.resize);
+
+    }
+    resize(e) {
+        document.body.appendChild(this.sizePreview);
+        this.sizePreview.style.left = e.clientX + 50 + "px";
+        this.sizePreview.style.top = e.clientY + "px";
+        if (this.clickedResizer.classList.contains('left-center')) {
+            let height = this.original_height + (e.pageY - this.original_mouse_y)
+            let width = this.original_width - (e.pageX - this.original_mouse_x)
+            let left = 0;
+            height = height + 'px';
+            this.onResize(this.element, { height })
+            if (width > this.minimum_size) {
+                width = width + 'px'
+                this.sizePreview.innerHTML = `w:${parseInt(width)}`;;
+                left = this.original_x + (e.pageX - this.original_mouse_x) + 'px';
+                this.onResize(this.element, { width, left })
+            }
+        }
+        else if (this.clickedResizer.classList.contains('right-center')) {
+            let width = this.original_width + (e.pageX - this.original_mouse_x);
+            if (width > this.minimum_size) {
+                width = width + 'px'
+                this.sizePreview.innerHTML = `w:${parseInt(width)}`;
+                this.onResize(this.element, { width })
+            }
+        }
+        else if (this.clickedResizer.classList.contains('bottom-center')) {
+            let height = this.original_height + (e.pageY - this.original_mouse_y)
+            height = height + 'px';
+            this.sizePreview.innerHTML = `h:${parseInt(height)}`;
+            this.onResize(this.element, { height })
+        }
+        else if (this.clickedResizer.classList.contains('bottom-right')) {
+            let width = this.original_width + (e.pageX - this.original_mouse_x);
+            let height = this.original_height + (e.pageY - this.original_mouse_y)
+            if (width > this.minimum_size) {
+                width = width + 'px';
+                this.sizePreview.innerHTML = `w:${parseInt(width)}`;;
+                this.onResize(this.element, { width })
+
+            }
+            height = height + 'px';
+            this.onResize(this.element, { height })
+        } else if (this.clickedResizer.classList.contains('bottom-left')) {
+            let height = this.original_height + (e.pageY - this.original_mouse_y)
+            let width = this.original_width - (e.pageX - this.original_mouse_x)
+            let left = 0;
+            height = height + 'px';
+            this.onResize(this.element, { height })
+            if (width > this.minimum_size) {
+                width = width + 'px'
+                this.sizePreview.innerHTML = `w:${parseInt(width)}`;;
+                left = this.original_x + (e.pageX - this.original_mouse_x) + 'px';
+                this.onResize(this.element, { width, left })
+            }
+        } else if (this.clickedResizer.classList.contains('top-right')) {
+            let width = this.original_width + (e.pageX - this.original_mouse_x);
+            if (width > this.minimum_size) {
+                width = width + 'px'
+                this.sizePreview.innerHTML = `w:${parseInt(width)}`;;
+                this.onResize(this.element, { width })
+
+            }
+
+        }
+        else if (this.clickedResizer.classList.contains('top-left')) {
+            let height = this.original_height + (e.pageY - this.original_mouse_y)
+            let width = this.original_width - (e.pageX - this.original_mouse_x)
+            let left = 0;
+            height = height + 'px';
+            this.onResize(this.element, { height })
+            if (width > this.minimum_size) {
+                width = width + 'px'
+                this.sizePreview.innerHTML = `w:${parseInt(width)}`;;
+                left = this.original_x + (e.pageX - this.original_mouse_x) + 'px';
+                this.onResize(this.element, { width, left })
+            }
+        }
+        this.rePosition();
+    }
+    rePosition() {
+        window.requestAnimationFrame(() => {
+            const rect = this.element.getBoundingClientRect();
+            this.borders.forEach(border => {
+                if (border.classList.contains('left')) {
+                    border.style.width = "1px";
+                    border.style.left = rect.left + 'px';
+                    border.style.top = rect.top + 'px';
+                    border.style.height = rect.height + 'px';
+                } else if (border.classList.contains('right')) {
+                    border.style.width = "1px";
+                    border.style.left = rect.right + 'px';
+                    border.style.top = rect.top + 'px';
+                    border.style.height = rect.height + 'px';
+                } else if (border.classList.contains('top')) {
+                    border.style.left = rect.left + 'px';
+                    border.style.top = rect.top + 'px';
+                    border.style.width = rect.width + 'px';
+                    border.style.height = "1px";
+                } else if (border.classList.contains('bottom')) {
+                    border.style.height = "1px";
+                    border.style.left = rect.left + 'px';
+                    border.style.top = rect.bottom + 'px';
+                    border.style.width = rect.width + 'px';
+                }
+            });
+            for (let i = 0; i < this.resizers.length; i++) {
+                const resizer = this.resizers[i];
+                if (resizer.classList.contains('bottom-left')) {
+                    resizer.style.left = rect.left + 'px';
+                    resizer.style.top = rect.bottom + 'px';
+                } else if (resizer.classList.contains('bottom-right')) {
+                    resizer.style.left = rect.right + 'px';
+                    resizer.style.top = rect.bottom + 'px';
+                } else if (resizer.classList.contains('top-right')) {
+                    resizer.style.left = rect.right + 'px';
+                    resizer.style.top = rect.top + 'px';
+                } else if (resizer.classList.contains('top-left')) {
+                    resizer.style.left = rect.left + 'px';
+                    resizer.style.top = rect.top + 'px';
+                } else if (resizer.classList.contains('left-center')) {
+                    resizer.style.left = rect.left + 'px';
+                    resizer.style.top = (rect.top + rect.bottom) / 2 + 'px';
+                } else if (resizer.classList.contains('right-center')) {
+                    resizer.style.left = rect.right + 'px';
+                    resizer.style.top = (rect.top + rect.bottom) / 2 + 'px';
+                } else if (resizer.classList.contains('bottom-center')) {
+                    resizer.style.left = (rect.left + rect.right) / 2 + 'px';
+                    resizer.style.top = rect.bottom + 'px';
+                }
+            }
+
+
+        })
+    }
+    createResizer = (className) => {
+        const resizer = document.createElement('div');
+        resizer.className = className;
+        resizer.classList.add(classes.resizer);
+        return resizer;
+    }
+}
+
+export default Resizable
